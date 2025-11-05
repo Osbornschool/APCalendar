b3d71f6383c41f9471805c9bd8c62bdd7479b648
Osbornschool <Osbornc@nycstudents.net>
2025-11-04 19:56:35 -0500

    Inital commit

---
+++ src/Main/APCalendar.java
@@ -0,0 +1,20 @@
+package Main;
+
+public class APCalendar {
+
+    private static boolean isLeapYear(int year){
+        return false;
+    }
+    public static int numberOfLeapYears(int year1, int year2){
+        return 0;
+    }
+    private static int firstDayOfYear(int year){
+        return 0;
+    }
+    private static int dayOfYear(int month, int day, int year){
+        return 0;
+    }
+    public static int dayOfWeek(int month, int day, int year){
+        return 0;
+    }
+}
